c7a8c6fb664e47ea35f07fa6717fe15409cca04a
StfnRomano <49978906+StfnRomano@users.noreply.github.com>
2023-09-21 10:57:47 +0200

    ask about eating (#4)
    complete questions

--- SRO.sh
+++ SRO.sh
@@ -8,4 +8,12 @@ then
 	echo "This is bad, you better learn how to like them. They are very cute!"
 else
 	echo "You rock, broccoli are cute!" 
+	echo "Do you eat them? [yes/no]"
+	read answer2
+	if [ "$answer2" == "yes" ]
+	then
+		echo "You are cruel, they are cute not tasty!"
+	else
+		echo "Well done, they are too cute for that"
+	fi
 fi
